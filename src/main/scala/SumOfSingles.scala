e2496987a66884e9aad0f8c634f3bc9e352a6bb3
Liam Henderson <liam.henderson@digital.hmrc.gov.uk>
2018-05-24 09:54:04 +0100

    complete kata with code and tests

--- src/main/scala/SumOfSingles.scala
+++ src/main/scala/SumOfSingles.scala
@@ -1,3 +1,12 @@
 object SumOfSingles {
 
+  def sum(input : List[Int]) : Int = {
+
+
+    val distinct = input.distinct
+    val repeatedNums = input diff distinct
+    val nonRepeatedNums = distinct diff repeatedNums
+    nonRepeatedNums.sum
+  }
+
 }
